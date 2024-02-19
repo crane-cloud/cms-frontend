98f78cc03d9f227fb6a8086363151eec5137229d
neelxie <kidricederek@gmail.com>
2024-02-19 17:34:14 +0300

    Add new fix for tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders landing page when visiting /', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>
+  );
+  const landingPageElement = screen.getByText(/open source/i);
+  expect(landingPageElement).toBeInTheDocument();
+});
+
+test('renders terms of service page when visiting /terms-of-service', () => {
+  render(
+    <MemoryRouter initialEntries={['/terms-of-service']}>
+      <App />
+    </MemoryRouter>
+  );
+  const termsPageElement = screen.getByText(/terms of service/i);
+  expect(termsPageElement).toBeInTheDocument();
 });
